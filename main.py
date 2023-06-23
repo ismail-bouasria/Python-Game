7ef60008998b3916bce7ad264e7cb459dd9f5884
ismail <ismail.bouasria@laplateforme.io>
2023-06-23 19:06:34 +0200

    update background of screen game

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ pygame.init()
 # Generer la fenêtre du jeu
 pygame.display.set_caption("Comet fall Game")
 screen = pygame.display.set_mode((1080, 720))
-clock = pygame.time.Clock()
 background = pygame.image.load('assets/bg.jpg')
 running = True
 
 # boucle tant que cette condition est vrai la fenêtre reste ouverte
 while running:
-    # si le joueur ferme la fenêtre
+    # appliquer l'arrière plan de notre jeu
+    screen.blit(background,(0,-200))
 
+    #mettre à jour l'écran
+    pygame.display.flip()
+
+    # si le joueur ferme la fenêtre
     for event in pygame.event.get():
         # que l'evenement est fermeture de fenetre.
         if event.type == pygame.QUIT:
