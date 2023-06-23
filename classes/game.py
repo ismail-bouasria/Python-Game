e997e4d97a68ea51b3a5729b58b8c7e78c137140
ismail <ismail.bouasria@laplateforme.io>
2023-06-23 20:46:04 +0200

    create classes Player/Game

---
+++ classes/game.py
@@ -0,0 +1,9 @@
+import pygame
+from player import Player
+
+# Créer une classe Game
+class Game:
+    def __init__(self):
+        #génerer notre joueur
+        self.player = Player()
+
--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # importer Pygame et l'initaliser
 import pygame
+from classes.game import Game
 pygame.init()
 
 # Generer la fenêtre du jeu
 pygame.display.set_caption("Comet fall Game")
 screen = pygame.display.set_mode((1080, 720))
+
+# Importer charger à l'arrière plan
 background = pygame.image.load('assets/bg.jpg')
+
+# Charger notre jeu
+game = Game()
+
 running = True
 
-# boucle tant que cette condition est vrai la fenêtre reste ouverte
+# Boucle tant que cette condition est vrai la fenêtre reste ouverte
 while running:
     # appliquer l'arrière plan de notre jeu
     screen.blit(background,(0,-200))
+
+    # appliquer l'image de mon joueur
+    screen.blit(game.player.image, game.player.rect)
 
     #mettre à jour l'écran
     pygame.display.flip()
