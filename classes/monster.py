aad88d4117d849f455e80f487bdb785ce0de029d
ismail <ismail.bouasria@laplateforme.io>
2023-06-24 17:43:57 +0200

    create classe Monster

---
+++ classes/monster.py
@@ -0,0 +1,9 @@
+# je crée ma classe monster
+import pygame
+
+
+class monster(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image =
+        self.velocity = 3
